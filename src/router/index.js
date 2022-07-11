f7072fc13297f7c8f2370d87bd9d86b3a4badacf
nimrod zelzer <zelzernim@gmail.com>
2022-07-11 14:46:19 +0300

    after cls clean

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHashHistory } from 'vue-router'
+import home from '../views/home.vue'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: home
     },
     {
       path: '/about',
@@ -15,7 +14,6 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     }
   ]
 })
---
+++ src/views/about.vue
@@ -0,0 +1,19 @@
+<template >
+    <h2>im about page</h2>
+</template>
+<script>
+export default {
+    template: `
+`,
+    data() {
+        return {};
+    },
+    created() { },
+    methods: {},
+    computed: {},
+    unmounted() { },
+};
+</script>
+<style >
+    
+</style>
